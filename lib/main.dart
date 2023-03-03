f9152fa53f23d493f07fe1bdcd6afba6cf5d682c
Валерий Судаков <sudakov.valera@list.ru>
2023-03-03 14:47:46 +0300

    fourth bloc lesson | learnt how to use context.read watch and select and how to render only what u want not the whole app

--- lib/main.dart
+++ lib/main.dart
@@ -26,8 +26,6 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final counterBloc = CounterBloc();
-    final userBloc = UserBloc();
     // To use block u need to wrap all widget that a going to use it in BlocProvider
     // It can use generic type
     //MultiblocProvider used when u need to use more than one block on a page
@@ -35,67 +33,86 @@ class MyHomePage extends StatelessWidget {
       providers: [
         BlocProvider<CounterBloc>(
           // u need to create method which are going to return instance of your bloc
-          create: (context) => counterBloc,
+          create: (context) => CounterBloc(),
           // in order to use states you need to wrap all widgets or one that are going
           // to be changed with BlocBuilder
           // see text widget
         ),
         BlocProvider<UserBloc>(
-          create: (context) => userBloc,
+          create: (context) => UserBloc(),
         ),
       ],
-      child: Scaffold(
-        floatingActionButton: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            IconButton(
-                onPressed: () => counterBloc.add(CounterIncEvent()),
-                icon: const Icon(Icons.plus_one)),
-            IconButton(
-                onPressed: () => counterBloc.add(CounterDecEvent()),
-                icon: const Icon(Icons.exposure_minus_1)),
-            IconButton(
-                onPressed: () =>
-                    userBloc.add(UserGetUserEvent(counterBloc.state)),
-                icon: const Icon(Icons.person)),
-            IconButton(
-                onPressed: () =>
-                    userBloc.add(UserGetUserJobEvent(counterBloc.state)),
-                icon: const Icon(Icons.work)),
-          ],
-        ),
-        body: SafeArea(
-          child: Center(
-            // u need to give blockbuilder your bloc and state (now it is int)
-            child: Column(
-              children: [
-                BlocBuilder<CounterBloc, int>(
-                  // and u need to specify your bloc
-                  bloc: counterBloc,
-                  builder: (context, state) {
-                    return Text(state.toString(),
-                        style: TextStyle(fontSize: 33));
+      child: Builder(builder: ((context) {
+        final counterBloc = BlocProvider.of<CounterBloc>(context);
+        final userBloc = BlocProvider.of<UserBloc>(context);
+        return Scaffold(
+          floatingActionButton: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              IconButton(
+                  onPressed: () {
+                    counterBloc.add(CounterIncEvent());
                   },
-                ),
-                BlocBuilder<UserBloc, UserState>(
-                  bloc: userBloc,
-                  builder: (context, state) {
-                    final users = state.users;
-                    final jobs = state.jobs;
-                    return Column(
-                      children: [
-                        if (state.isLoading) CircularProgressIndicator(),
-                        if (users.isNotEmpty) ...users.map((e) => Text(e.name)),
-                        if (jobs.isNotEmpty) ...jobs.map((e) => Text(e.name))
-                      ],
-                    );
+                  icon: const Icon(Icons.plus_one)),
+              IconButton(
+                  onPressed: () {
+                    counterBloc.add(CounterDecEvent());
                   },
-                ),
-              ],
+                  icon: const Icon(Icons.exposure_minus_1)),
+              IconButton(
+                  onPressed: () {
+                    userBloc.add(
+                        UserGetUserEvent(context.read<CounterBloc>().state));
+                  },
+                  icon: const Icon(Icons.person)),
+              IconButton(
+                  onPressed: () {
+                    userBloc.add(
+                        UserGetUserJobEvent(context.read<CounterBloc>().state));
+                  },
+                  icon: const Icon(Icons.work)),
+            ],
+          ),
+          body: SafeArea(
+            child: Center(
+              // u need to give blockbuilder your bloc and state (now it is int)
+              child: Column(
+                children: [
+                  BlocBuilder<CounterBloc, int>(
+                    // and u need to specify your bloc
+                    // bloc: counterBloc,
+                    builder: (context, state) {
+                      final users = context.select(
+                        (UserBloc bloc) => bloc.state.users,
+                      );
+                      return Column(
+                        children: [
+                          Text(state.toString(),
+                              style: TextStyle(fontSize: 33)),
+                          if (users.isNotEmpty)
+                            ...users.map((e) => Text(e.name)),
+                        ],
+                      );
+                    },
+                  ),
+                  BlocBuilder<UserBloc, UserState>(
+                    //bloc: userBloc,
+                    builder: (context, state) {
+                      final jobs = state.jobs;
+                      return Column(
+                        children: [
+                          if (state.isLoading) CircularProgressIndicator(),
+                          if (jobs.isNotEmpty) ...jobs.map((e) => Text(e.name))
+                        ],
+                      );
+                    },
+                  ),
+                ],
+              ),
             ),
           ),
-        ),
-      ),
+        );
+      })),
     );
   }
 }
